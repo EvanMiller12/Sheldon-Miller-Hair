c5eaf1b93e369324591a0ecdef70d17f29a48ac0
evan miller <evanchancemiller@gmail.com>
2019-02-14 10:37:50 -0500

    feat: mobile nav complete functionality

--- app/client/components/Nav.js
+++ app/client/components/Nav.js
@@ -2,22 +2,6 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const StyledNav = styled.nav`
-  #mobile-nav {
-    display: none;
-
-    .nav-link {
-      margin: 0 1.5rem;
-
-      :last-child {
-        margin-right: 0;
-      }
-
-      :hover {
-        color: ${props => props.theme.coral};
-      }
-    }
-  }
-
   .nav-link,
   .logo {
     text-decoration: none;
@@ -37,16 +21,11 @@ const StyledNav = styled.nav`
     font-size: 2em;
   }
 
-  .active {
-    display: block;
-  }
-
-
-    #notActive {
-      display: none
+    .main-nav {
+      display: none;
     }
-
-    #active {
+    
+    .responsive-nav {
       display: flex;
       flex-direction: column;
       align-items: flex-end;
@@ -54,7 +33,7 @@ const StyledNav = styled.nav`
       padding: 20px;
       /* background-color: ${props => props.theme.lightGrey}; */
       border: 1px solid ${props => props.theme.lightGrey};
-      a {
+      .nav-link {
         color: ${props => props.theme.coral};
         font-weight: 400;
         margin-bottom: 10px;
@@ -70,16 +49,27 @@ const StyledNav = styled.nav`
       height: 120px;
       align-items: center;
     }
-
-    #mobile-nav {
+    
+    .responsive-nav,
+    .main-nav {
       display: flex;
-      margin-right: 30px;
       flex-direction: row;
-      justify-content: flex-end;
-    }
+      align-items: flex-end;
+      margin-right: 30px;
+      border: none;
 
-    .nav-link {
-      margin-left: 0 0.5rem;
+      .nav-link {
+      margin: 0 1.5rem;
+      color: ${props => props.theme.navy};
+
+      :last-child {
+        margin-right: 0;
+      }
+
+      :hover {
+        color: ${props => props.theme.coral};
+      }
+    }
     }
 
     .logo {
@@ -104,7 +94,8 @@ class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: "notActive"
+      active: false,
+      nav: "main-nav"
     };
   }
 
@@ -112,7 +103,18 @@ class Nav extends React.Component {
     const { active } = this.state;
 
     this.setState({
-      active: active === "notActive" ? "active" : "notActive"
+      active: !active,
+      nav: active ? "responsive-nav" : "main-nav"
+    });
+  }
+
+  hideMenuOnNav() {
+    const { nav } = this.state;
+
+    if (nav === "main-nav") return;
+
+    this.setState({
+      nav: "main-nav"
     });
   }
   render() {
@@ -125,11 +127,7 @@ class Nav extends React.Component {
           <Link href="#">
             <a className="logo">Sheldon Miller Hair</a>
           </Link>
-          <div
-            onClick={() => this.toggleMenu()}
-            className="mobile-nav"
-            id={this.state.active}
-          >
+          <div onClick={() => this.hideMenuOnNav()} className={this.state.nav}>
             <Link href="/mystory">
               <a className="nav-link">My Story</a>
             </Link>
@@ -138,9 +136,6 @@ class Nav extends React.Component {
             </Link>
             <Link href="/mywork">
               <a className="nav-link">My Work</a>
-            </Link>
-            <Link href="/theblog">
-              <a className="nav-link">The Blog</a>
             </Link>
             <Link href="/contact">
               <a className="nav-link">Contact Me</a>
---
+++ app/client/pages/bookNow.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const BookNow = () => (
+  <div>
+    <h1>Book Now</h1>
+  </div>
+);
+export default BookNow;
